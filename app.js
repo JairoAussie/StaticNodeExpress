925d8b65310d04cefa9f0d0870fd577d639930c1
Jairo Bilbao <jairoaussie@gmail.com>
2019-08-02 09:22:43 +1000

    Adding comments, meeting expectations

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
+//constants to load modules and get the json file
 const express = require('express');
 const data = require('./data.json');
 const projects = data.projects;
+//create an express app
 const app = express();
 
+//serve static files
 app.use('/static', express.static('public'));
-
+//set pug to template enfine
 app.set('view engine', 'pug');
 
+//get index, send the projects
 app.get('/', (req, res) => {
     res.render('index',{projects});
 });
-
+//get about
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
+//get projects, each project has its own id, so it receives its own data
 app.get('/project/:id', (req, res) => {
     res.render('project', {
         project_name: projects[req.params.id].project_name,
@@ -26,7 +31,7 @@ app.get('/project/:id', (req, res) => {
     });
 });
 
-
+//handling errors
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
@@ -38,7 +43,7 @@ app.use((err, req, res, next) => {
   res.status(err.status);
   res.render('error');
 });
-
+//app working in the port 3000
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
 });
