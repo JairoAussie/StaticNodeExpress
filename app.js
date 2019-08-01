c83a7bbc028754d528f38565ad8cccb0819c7cb5
Jairo Bilbao <jairoaussie@gmail.com>
2019-08-02 09:16:00 +1000

    project pages work

--- app.js
+++ app.js
@@ -15,8 +15,15 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/project', (req, res) => {
-    res.render('project');
+app.get('/project/:id', (req, res) => {
+    res.render('project', {
+        project_name: projects[req.params.id].project_name,
+        description: projects[req.params.id].description,
+        technologies: projects[req.params.id].technologies,
+        live_link: projects[req.params.id].live_link,
+        github_link: projects[req.params.id].github_link,
+        image_urls: projects[req.params.id].image_urls
+    });
 });
 
 
